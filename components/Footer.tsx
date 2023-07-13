84b3e2c93cb5832d348d986b555fd2ba4240d438
Nofrialdi <aldi.fasive@gmail.com>
2023-07-13 12:20:31 +0700

    add Contact Page, Menu detail Page, Header, Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,146 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div>
+      <div className="flex  w-full  bg-white">
+        <footer className="w-full text-gray-700 bg-[#F7FBF9] body-font">
+          <div className="container flex flex-col flex-wrap px-5 py-24 mx-auto md:items-center lg:items-start md:flex-row md:flex-no-wrap">
+            <div className="flex-shrink-0 w-64 mx-auto text-center md:mx-0 md:text-left">
+              <div className="flex flex-row">
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="46"
+                    height="41"
+                    viewBox="0 0 46 41"
+                    fill="none"
+                  >
+                    <path
+                      d="M20.5897 1.52081L0.906135 20.9794C-0.302045 22.1738 -0.302045 24.1102 0.906135 25.3047C2.11356 26.4976 4.07232 26.4976 5.2805 25.3047L22.7765 8.0065L40.2724 25.3047C41.4806 26.4976 43.4394 26.4976 44.6468 25.3047C45.855 24.1102 45.855 22.1738 44.6468 20.9794L24.9633 1.52081C23.7558 0.326396 21.7971 0.326396 20.5897 1.52081Z"
+                      fill="#548776"
+                    />
+                    <path
+                      d="M34.8394 25.4301C34.5034 23.9699 32.8338 23.3981 31.4678 24.0431C30.1018 24.688 29.5868 26.3259 29.6586 27.8204C29.6714 28.0757 29.6691 28.331 29.6518 28.5878C29.5755 29.7509 29.1979 30.8751 28.5561 31.8531C27.9143 32.8295 27.0293 33.6283 25.9865 34.1717C24.9436 34.7152 23.777 34.9839 22.5983 34.9541C21.4195 34.9242 20.2687 34.5942 19.2554 34C18.242 33.4043 17.4001 32.5607 16.8103 31.5515C16.2206 30.5422 15.9027 29.4015 15.8868 28.2355C15.8838 27.9787 15.8944 27.7234 15.92 27.4695C16.0703 25.9795 15.6414 24.3178 14.3109 23.6041C12.9804 22.8904 11.2837 23.3757 10.8714 24.8164C10.6773 25.4958 10.5429 26.19 10.4696 26.8917C10.4206 27.3605 10.3994 27.8338 10.4055 28.3086C10.4341 30.4003 11.005 32.4518 12.0644 34.2628C13.1231 36.0753 14.6348 37.5878 16.4539 38.6582C18.2737 39.7273 20.3405 40.317 22.4563 40.3707C24.5721 40.426 26.6676 39.9422 28.5402 38.9673C30.4137 37.9924 32.0017 36.5576 33.1547 34.8033C34.307 33.0475 34.9844 31.0289 35.1218 28.9402C35.2003 27.7577 35.1037 26.5752 34.8394 25.4301Z"
+                      fill="#548776"
+                    />
+                  </svg>
+                </div>
+                <div>
+                  <h1 className="px-3 text-3xl font-bold">WAREG</h1>
+                </div>
+              </div>
+
+              <p className="mt-2 text-sm font-normal not-italic leading-6 text-[#005B33] ">
+                Wareg adalah sebuah restoran yang menyajikan masakan rumahan
+                dengan harga terjangkau. Kami selalu menyajikan menu yang segar
+                dan berganti setiap minggunya
+              </p>
+            </div>
+            <div className="flex flex-wrap flex-grow mt-10 -mb-10 text-center md:pl-20 md:mt-0 md:text-left">
+              <div className="w-full px-4 lg:w-1/4 md:w-1/2">
+                <h2 className="mb-3  text-sm font-bold tracking-widest text-[#005B33] uppercase title-font">
+                  About
+                </h2>
+                <nav className="mb-10 list-none">
+                  <li className="mt-3">
+                    <a className="text-[#005B33] cursor-pointer hover:text-gray-900">
+                      Feature
+                    </a>
+                  </li>
+                  <li className="mt-3">
+                    <a className="text-[#005B33] cursor-pointer hover:text-gray-900">
+                      FAQ'S
+                    </a>
+                  </li>
+                  <li className="mt-3">
+                    <a className="text-[#005B33] cursor-pointer hover:text-gray-900">
+                      News
+                    </a>
+                  </li>
+
+                  <li className="mt-3">
+                    <a className="text-[#005B33] cursor-pointer hover:text-gray-900">
+                      Pricing
+                    </a>
+                  </li>
+                </nav>
+              </div>
+              <div className="w-full px-4 lg:w-1/4 md:w-1/2">
+                <h2 className="mb-3 text-sm font-bold tracking-widest text-[#005B33] uppercase title-font">
+                  Company
+                </h2>
+                <nav className="mb-10 list-none">
+                  <li className="mt-3">
+                    <a className="text-[#005B33] cursor-pointer hover:text-gray-900">
+                      Care Values
+                    </a>
+                  </li>
+                  <li className="mt-3">
+                    <a className="text-[#005B33] cursor-pointer hover:text-gray-900">
+                      Partner
+                    </a>
+                  </li>
+                  <li className="mt-3">
+                    <a className="text-[#005B33] cursor-pointer hover:text-gray-900">
+                      Blog
+                    </a>
+                  </li>
+                  <li className="mt-3">
+                    <a className="text-[#005B33] cursor-pointer hover:text-gray-900">
+                      Contact
+                    </a>
+                  </li>
+                </nav>
+              </div>
+              <div className="w-full px-4 lg:w-1/4 md:w-1/2">
+                <h2 className="mb-3 text-sm font-bold tracking-widest  text-[#005B33] uppercase title-font">
+                  Support
+                </h2>
+                <nav className="mb-10 list-none">
+                  <li className="mt-3">
+                    <a className="text-[#005B33] cursor-pointer hover:text-gray-900">
+                      Support Center
+                    </a>
+                  </li>
+                  <li className="mt-3">
+                    <a className="text-[#005B33] cursor-pointer hover:text-gray-900">
+                      Feedback
+                    </a>
+                  </li>
+                  <li className="mt-3">
+                    <a className="text-[#005B33] cursor-pointer hover:text-gray-900">
+                      Accessibility
+                    </a>
+                  </li>
+                </nav>
+              </div>
+              <div className="w-full px-4 lg:w-1/4 md:w-1/2">
+                <h2 className="mb-3 text-sm font-bold tracking-widest text-[#005B33] uppercase title-font">
+                  Get in Touch
+                </h2>
+                <nav className="mb-10 list-none">
+                  <li className="mt-3">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      placeholder="Your email hare..."
+                      required
+                      className="mb-5 block p-2.5 w-full text-sm bg-gray-50 rounded-full border border-[#005B33] "
+                    />
+                  </li>
+                  <li className="mt-3">
+                    <button className="middle none center w-full rounded-full bg-[#FABB3D] border-white py-3 px-6 font-sans text-xs font-bold uppercase text-[#005B33] shadow-md  transition-all hover:shadow-lg  focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none ">
+                      Contact Us
+                    </button>
+                  </li>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </div>
+  );
+}
---
+++ pages/menu/[id].tsx
@@ -0,0 +1,137 @@
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+import React, { useState } from 'react';
+
+export default function DetailMenu() {
+  const [images, setImages] = useState({
+    img1: 'https://www.masakapahariini.com/wp-content/uploads/2021/07/Nasi-Goreng-Spesial-Ayam-Kecombrang.jpg',
+    img2: 'https://sweetrip.id/wp-content/uploads/2020/06/miejiwo_101049629_258573461864967_2414588224721876492_n.jpg',
+    img3: 'https://awsimages.detik.net.id/community/media/visual/2022/08/07/resep-nasi-goreng-ayam-dan-ebi_43.jpeg?w=1200',
+    img4: 'https://global-uploads.webflow.com/643707ac57bf42445298b82e/643f8840b2e0a7512e675eb5_nasi_goreng_special.webp',
+  });
+
+  const [activeImg, setActiveImage] = useState(images.img1);
+
+  const [amount, setAmount] = useState(1);
+
+  const [count, setCount] = useState(0);
+
+  const addCountHandler = () => {
+    setCount(count + 1);
+  };
+  const removeCountHandler = () => {
+    if (count === 0) {
+      return;
+    }
+    setCount(count - 1);
+  };
+  return (
+    <div>
+      <Header />
+      <div className="max-w-5xl mx-auto p-8">
+        <div className="flex flex-col justify-between lg:flex-row gap-16 lg:items-center">
+          <div className="flex flex-col gap-6 lg:w-2/4">
+            <img
+              src={activeImg}
+              alt=""
+              className="w-full h-full aspect-square object-cover rounded-xl"
+            />
+            <div className="flex flex-row justify-between h-24">
+              <img
+                src={images.img1}
+                alt=""
+                className="w-24 h-24 rounded-md cursor-pointer"
+                onClick={() => setActiveImage(images.img1)}
+              />
+              <img
+                src={images.img2}
+                alt=""
+                className="w-24 h-24 rounded-md cursor-pointer"
+                onClick={() => setActiveImage(images.img2)}
+              />
+              <img
+                src={images.img3}
+                alt=""
+                className="w-24 h-24 rounded-md cursor-pointer"
+                onClick={() => setActiveImage(images.img3)}
+              />
+              <img
+                src={images.img4}
+                alt=""
+                className="w-24 h-24 rounded-md cursor-pointer"
+                onClick={() => setActiveImage(images.img4)}
+              />
+            </div>
+          </div>
+          {/* ABOUT */}
+          <div className="flex flex-col gap-4 lg:w-2/4">
+            <div>
+              <h1 className="text-4xl font-bold">Nasi Goreng Spesial </h1>
+            </div>
+            <h6 className="text-emerald-600 text-2xl font-semibold">
+              Rp. 25.000
+            </h6>
+            <div className="flex flex-row gap-2">
+              <p>Category : </p>
+              <p className="font-bold">Makanan</p>
+            </div>
+
+            <div className="flex flex-row gap-2">
+              <p>Calories :</p>
+              <p className="font-bold">257 Calories</p>
+            </div>
+            <p className="text-gray-700">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+              nostrum amet at laboriosam laborum voluptatum veniam, reiciendis
+              modi minima et consequatur iure iusto totam voluptates dignissimos
+              corrupti cupiditate deleniti non.
+            </p>
+
+            <div className="flex flex-row items-center gap-12">
+              <div className="flex flex-row items-center">
+                <button
+                  className="bg-gray-200 py-2 px-4 rounded-lg text-emerald-800 text-3xl"
+                  onClick={removeCountHandler}
+                >
+                  -
+                </button>
+                <span className="py-4 px-6 rounded-lg"> {count}</span>
+
+                <button
+                  className="bg-gray-200 py-2 px-4 rounded-lg text-emerald-600 text-3xl"
+                  onClick={addCountHandler}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+            <button className="bg-emerald-600 text-white font-semibold py-3 px-16 rounded-xl h-full">
+              Add to Cart
+            </button>
+          </div>
+        </div>
+        {/* Deskripsi */}
+        <div>
+          <div className="py-5">
+            <h1 className="text-4xl font-bold">Deskripsi</h1>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad maiores
+            officia, minus eligendi mollitia, natus architecto, asperiores
+            expedita dolore velit laudantium officiis consectetur laborum.
+            Similique suscipit laboriosam ratione repellat modi?
+          </p>
+        </div>
+
+        <div className=" my-5 flex flex-row justify-between">
+          <h1 className="text-4xl font-bold">Anda Mungkin Menyukai</h1>
+
+          <button className="bg-emerald-600 text-white font-semibold py-3 px-16 rounded-xl">
+            View More
+          </button>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
