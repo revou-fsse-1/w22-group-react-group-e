6eff7bfedd217707ff7c578b30256b3b76da51cf
meselghea <122159476+meselghea@users.noreply.github.com>
2023-07-17 18:39:10 +0700

    fix menupage (filter, menucard)

--- components/MenuCard.tsx
+++ components/MenuCard.tsx
@@ -1,84 +1,64 @@
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import StarRating from 'react-star-rating-component';
 
 interface MenuCardProps {
+  id: number;
   name: string;
   price: number;
-  rating: number;
-  image: string;
   category: string;
-}
-
-const MenuCard: React.FC<MenuCardProps> = ({ name, price, rating, image, category }) => {
-  const renderRatingStars = (rating: number) => {
-    const fullStars = Math.floor(rating);
-    const halfStars = Math.ceil(rating - fullStars);
-    const emptyStars = 5 - fullStars - halfStars;
-
-    return (
-      <div className="flex">
-        {[...Array(fullStars)].map((_, index) => (
-          <StarIcon key={`full-${index}`} />
-        ))}
-        {[...Array(halfStars)].map((_, index) => (
-          <HalfStarIcon key={`half-${index}`} />
-        ))}
-        {[...Array(emptyStars)].map((_, index) => (
-          <EmptyStarIcon key={`empty-${index}`} />
-        ))}
-      </div>
-    );
+  ratings: number[];
+  menuImages: {
+    img1: string;
+    img2: string;
+    img3: string;
+    img4: string;
   };
+}
+  
+const MenuCard: React.FC<MenuCardProps> = ({
+  id,
+  name,
+  price,
+  category,
+  ratings,
+  menuImages,
+}) => {
+  const averageRating = ratings.length > 0 ? ratings.reduce((sum, rating) => sum + rating, 0) / ratings.length : 0;
 
   return (
-    <div className="p-4 mb-4 border">
-      <img src={image} alt={name} className="mb-2" />
-      <div className="flex justify-between">
-        <div>
-          <h4 className="text-lg font-semibold">{name}</h4>
-          <p className="text-gray-600">Rating: {renderRatingStars(rating)}</p>
-          <p className="text-gray-600">Category: {category}</p>
+      <div className="p-4 w-[270px] h-[320px] bg-neutral-100 rounded-xl">
+      {menuImages && menuImages.img1 ? (
+        <Image className="w-[270px] h-[160px] rounded-tl-xl rounded-tr-xl" src={menuImages.img1} alt={name} />
+        ) : (
+          <div className="w-[270px] h-[160px] rounded-tl-xl rounded-tr-xl flex items-center justify-center text-gray-500">
+          No Picture
         </div>
-        <div>
-          <p className="text-lg font-semibold">${price}</p>
-          <button className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600">
-            Add to Cart
-          </button>
-        </div>
+      )}
+      <div className="">
+        <p className="text-[#333333] text-m font-normal leading-loose">{category}</p>
+        <Link href={`/menu/${id}`}>
+          <h3 className="text-[#333333] text-l font-bold leading-loose">{name}</h3>
+        </Link>
+        <StarRating
+          name={`rating-${id}`}
+          value={averageRating}
+          starCount={5}
+          starColor="#FFC107"
+          emptyStarColor="#E2E8F0"
+          editing={false}
+          />
+           <div className="flex items-center justify-between">
+          <p className="font-normal text-black text-m">Price: Rp{price.toLocaleString()}</p>
+          <button className="w-[100px] h-[37px] bg-[#548776] text-white rounded-[18.50px]">Add to Cart</button>
+          </div>
       </div>
     </div>
   );
 };
 
-const StarIcon: React.FC = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4">
-    <path
-      fillRule="evenodd"
-      d="M10 0L3.061 6.69 0 7.772l4.273 4.158-.941 5.484L10 14.468l4.668 2.946-.942-5.484L20 7.772l-3.06-1.082L10 0zm0 1.698l2.539 5.534 5.708.803-4.139 4.033.976 5.693L10 14.055l-4.084 2.714.976-5.693-4.14-4.033 5.708-.803L10 1.697z"
-      clipRule="evenodd"
-    />
-  </svg>
-);
-
-const HalfStarIcon: React.FC = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4">
-    <path
-      fillRule="evenodd"
-      d="M10 0L7.35 6.607 0 7.772l5.014 4.883L3.452 20 10 15.213 16.548 20l-1.562-7.394L20 7.772l-7.35-1.165L10 0zm0 1.698l2.453 5.315 5.32.75-3.862 3.776.911 5.337L10 14.938l-4.823 3.179.911-5.336-3.862-3.776 5.32-.751L10 1.698z"
-      clipRule="evenodd"
-    />
-  </svg>
-);
-
-const EmptyStarIcon: React.FC = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4">
-    <path
-      fillRule="evenodd"
-      d="M10 0L7.35 6.607 0 7.772l5.014 4.883L3.452 20 10 15.213 16.548 20l-1.562-7.394L20 7.772l-7.35-1.165L10 0zm0 1.698l2.453 5.315 5.32.75-3.862 3.776.911 5.337L10 14.938l-4.823 3.179.911-5.336-3.862-3.776 5.32-.751L10 1.698z"
-      clipRule="evenodd"
-    />
-  </svg>
-);
-
 export default MenuCard;
+
 
 
--- pages/menu/index.tsx
+++ pages/menu/index.tsx
@@ -1,20 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import Head from 'next/head';
-import MenuCard from '../../components/MenuCard';
-import Filter from '../../components/Filter';
+import React, { useEffect, useState } from 'react';
+import Filter from '@/components/Filter';
+import MenuCard from '@/components/MenuCard';
 
 interface Menu {
   id: number;
   name: string;
   price: number;
-  rating: number;
-  image: string;
-  category: string;
+  categoryId: string;
+  userId: number;
+  createdAt: string;
+  updatedAt: string;
+  calories: string;
+  description: string;
+  category: {
+    name: string;
+  };
+  ratings: {
+    rating: number;
+  }[];
+  menuImages: {
+    img1: string;
+    img2: string;
+    img3: string;
+    img4: string;
+  };
 }
 
 const MenuPage: React.FC = () => {
   const [menus, setMenus] = useState<Menu[]>([]);
-  const [selectedCategory, setSelectedCategory] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState<string[]>([]);
+  const [selectedRating, setSelectedRating] = useState<number | null>(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const menusPerPage = 10;
 
   useEffect(() => {
     fetchMenus();
@@ -22,53 +39,93 @@ const MenuPage: React.FC = () => {
 
   const fetchMenus = async () => {
     try {
-      const res = await fetch('https://example.com/menus'); // Ganti URL API sesuai kebutuhan Anda
-      const data = await res.json();
-      setMenus(data);
+      const response = await fetch('https://w17-wareg.onrender.com/menus');
+      const data = await response.json();
+      setMenus(data.menus);
     } catch (error) {
       console.error('Error fetching menus:', error);
     }
   };
 
   const handleCategoryChange = (category: string) => {
-    setSelectedCategory(category);
+    const selectedCategories = selectedCategory.includes(category)
+      ? selectedCategory.filter((item) => item !== category)
+      : [...selectedCategory, category];
+    setSelectedCategory(selectedCategories);
+  };
+
+  const handleRatingChange = (rating: number | null) => {
+    setSelectedRating(rating);
   };
 
   const filteredMenus = menus.filter((menu) => {
-    if (selectedCategory === 'All' || menu.category === selectedCategory) {
-      return true;
-    }
-    return false;
+    const categoryMatch =
+      selectedCategory.length === 0 || selectedCategory.includes(menu.category.name);
+    const ratingMatch =
+      selectedRating === null ||
+      (menu.ratings.length > 0 &&
+        Math.floor(
+          menu.ratings.reduce((sum, rating) => sum + rating.rating, 0) / menu.ratings.length
+        ) === selectedRating);
+
+    return categoryMatch && ratingMatch;
   });
 
+  // Calculate pagination
+  const indexOfLastMenu = currentPage * menusPerPage;
+  const indexOfFirstMenu = indexOfLastMenu - menusPerPage;
+  const currentMenus = filteredMenus.slice(indexOfFirstMenu, indexOfLastMenu);
+
+  const paginate = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
-    <div>
-      <Head>
-        <title>Menu Page</title>
-        <meta name="description" content="Page for displaying menus" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>Menu Page</h1>
-      <Filter
-        categories={['All', 'Nasi', 'Daging', 'Sayur', 'Minuman', 'Cemilan', 'Mie', 'Ikan']}
-        selectedCategory={selectedCategory}
-        onCategoryChange={handleCategoryChange}
-      />
-      <div className="grid">
-        {filteredMenus.map((menu) => (
+    <div className="flex">
+      <div className="w-1/5 p-4">
+        <Filter
+          categories={['Nasi', 'Daging', 'Sayur', 'Minuman', 'Cemilan', 'Ikan']}
+          selectedCategory={selectedCategory}
+          onCategoryChange={handleCategoryChange}
+          onRatingChange={handleRatingChange}
+          selectedRating={selectedRating}
+        />
+      </div>
+      <div className="flex flex-wrap justify-center w-4/5 gap-5 p-5 ">
+        {currentMenus.map((menu) => (
           <MenuCard
             key={menu.id}
+            id={menu.id}
             name={menu.name}
             price={menu.price}
-            rating={menu.rating}
-            image={menu.image}
-            category={menu.category}
+            category={menu.category.name}
+            ratings={menu.ratings.map((rating) => rating.rating)}
+            menuImages={menu.menuImages}
           />
         ))}
+      </div>
+      <div className="flex items-center justify-center mt-5">
+        {filteredMenus.length > menusPerPage && (
+          <ul className="pagination">
+            {Array(Math.ceil(filteredMenus.length / menusPerPage))
+              .fill(null)
+              .map((_, index) => (
+                <li
+                  key={index}
+                  className={`pagination-item ${index + 1 === currentPage ? 'active' : ''}`}
+                  onClick={() => paginate(index + 1)}
+                >
+                  {index + 1}
+                </li>
+              ))}
+          </ul>
+        )}
       </div>
     </div>
   );
 };
 
 export default MenuPage;
+
+
 
