d8f1438c1e7cec8843f0d80b3608672dfbe8abb0
anggr <anggorookky@gmail.com>
2023-07-18 21:35:49 +0800

    feat: add WeeklyMenu component to index page

--- components/Landingpage/WeeklyMenu.tsx
+++ components/Landingpage/WeeklyMenu.tsx
@@ -28,6 +28,38 @@ interface Menu {
   };
 }
 
+interface ArrowProps {
+  className?: string;
+  style?: React.CSSProperties;
+  onClick?: () => void;
+}
+
+function NextArrow(props: ArrowProps) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={`${className} custom-arrow text-green-500`}
+      style={{ ...style, display: 'block' }}
+      onClick={onClick}
+    >
+      &gt;
+    </div>
+  );
+}
+
+function PrevArrow(props: ArrowProps) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={`${className} custom-arrow text-green-500`}
+      style={{ ...style, display: 'block' }}
+      onClick={onClick}
+    >
+      &lt;
+    </div>
+  );
+}
+
 const WeeklyMenu: React.FC = () => {
   const [menus, setMenus] = useState<Menu[]>([]);
 
@@ -36,7 +68,7 @@ const WeeklyMenu: React.FC = () => {
       try {
         const response = await fetch('https://w17-wareg.onrender.com/menus');
         const data = await response.json();
-        setMenus(data.menus.slice(0, 6)); 
+        setMenus(data.menus.slice(0, 6));
       } catch (error) {
         console.error('Error fetching menus:', error);
       }
@@ -51,26 +83,38 @@ const WeeklyMenu: React.FC = () => {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 3,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
   };
 
   return (
-    <div className="p-10">
-      <h2 className="text-xl font-bold mb-4">Weekly Menu</h2>
-      <Slider {...settings}>
-        {menus.map((menu) => (
-          <div key={menu.id}>
-            <MenuCard
-              id={menu.id}
-              name={menu.name}
-              price={menu.price}
-              category={menu.category.name}
-              ratings={menu.ratings.map((rating) => rating.rating)}
-              menuImages={menu.menuImages}
-            />
-          </div>
-        ))}
-      </Slider>
-    </div>
+    <section
+      className="flex items-center w-full h-64 py-10 lg:py-60 lg:h-64 lg:mb-14"
+      style={{ backgroundColor: '#EEF6F4' }}
+    >
+      <div className="px-5 pt-6 mx-auto lg:max-w-6xl lg:px-0">
+        <div className="lg:max-w-6xl lg:px-0 ">
+          <h2 className="text-xl font-bold mb-4">Menu Minggu Ini</h2>
+          <Slider
+            {...settings}
+            className="grid grid-cols-1 gap-6 space-y-0 lg:gap-24 lg:grid-cols-3"
+          >
+            {menus.map((menu) => (
+              <div key={menu.id} className="p-1 mx-auto">
+                <MenuCard
+                  id={menu.id}
+                  name={menu.name}
+                  price={menu.price}
+                  category={menu.category.name}
+                  ratings={menu.ratings.map((rating) => rating.rating)}
+                  menuImages={menu.menuImages}
+                />
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </section>
   );
 };
 
