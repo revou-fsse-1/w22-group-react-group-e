153f9a7345883c7be2236bf9d47cea72a1948ab4
anggr <anggorookky@gmail.com>
2023-07-18 19:03:03 +0800

    fix: Fix typo in menuImages length check

--- pages/menu/[id].tsx
+++ pages/menu/[id].tsx
@@ -1,21 +1,67 @@
-import Footer from '@/components/Footer';
-import Header from '@/components/Header';
-import React, { useState } from 'react';
-import Image from 'next/image';
+import axios from 'axios';
+import { GetServerSideProps, NextComponentType } from 'next';
+import React, { useEffect, useState } from 'react';
+import { checkLogin } from '@/libs/checkLogin';
+import MenuCard from '@/components/MenuCard';
 
-export default function DetailMenu() {
-  const [images, setImages] = useState({
-    img1: 'https://www.masakapahariini.com/wp-content/uploads/2021/07/Nasi-Goreng-Spesial-Ayam-Kecombrang.jpg',
-    img2: 'https://sweetrip.id/wp-content/uploads/2020/06/miejiwo_101049629_258573461864967_2414588224721876492_n.jpg',
-    img3: 'https://awsimages.detik.net.id/community/media/visual/2022/08/07/resep-nasi-goreng-ayam-dan-ebi_43.jpeg?w=1200',
-    img4: 'https://global-uploads.webflow.com/643707ac57bf42445298b82e/643f8840b2e0a7512e675eb5_nasi_goreng_special.webp',
-  });
+interface ResGetProps {
+  id: number;
+  name: string;
+  price: number;
+  categoryId: string;
+  userId: number;
+  createdAt: string;
+  updatedAt: string;
+  calories: string;
+  description: string;
+  category: {
+    name: string;
+  };
+  ratings: {
+    rating: number;
+  }[];
+  menuImages: {
+    img1: string;
+    img2: string;
+    img3: string;
+    img4: string;
+  };
+}
+
+const DetailMenu: NextComponentType<any, any, ResGetProps> = (props: any) => {
+  const {
+    id,
+    name,
+    price,
+    calories,
+    description,
+    category,
+    ratings,
+    menuImages,
+  } = props;
+
+  const images = menuImages?.lenght
+    ? menuImages
+    : {
+        img1:
+          menuImages?.img1 ||
+          'https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930',
+        img2:
+          menuImages?.img2 ||
+          'https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930',
+        img3:
+          menuImages?.img3 ||
+          'https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930',
+        img4:
+          menuImages?.img4 ||
+          'https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930',
+      };
 
   const [activeImg, setActiveImage] = useState(images.img1);
-
   const [amount, setAmount] = useState(1);
-
   const [count, setCount] = useState(0);
+
+  const [menus, setMenus] = useState<ResGetProps[]>([]);
 
   const addCountHandler = () => {
     setCount(count + 1);
@@ -26,36 +72,56 @@ export default function DetailMenu() {
     }
     setCount(count - 1);
   };
+
+  const addToCart = () => {
+    checkLogin();
+  };
+
+  useEffect(() => {
+    fetchMenus();
+  }, []);
+
+  const fetchMenus = async () => {
+    try {
+      const response = await fetch(
+        `https://w17-wareg.onrender.com/menus?q=${category?.name}`,
+      );
+      const data = await response.json();
+      setMenus(data.menus);
+    } catch (error) {
+      console.error('Error fetching menus:', error);
+    }
+  };
   return (
     <div>
-      <div className="max-w-5xl p-8 mx-auto">
-        <div className="flex flex-col justify-between gap-16 lg:flex-row lg:items-center">
+      <div className="max-w-5xl mx-auto p-8">
+        <div className="flex flex-col justify-between lg:flex-row gap-16 lg:items-center">
           <div className="flex flex-col gap-6 lg:w-2/4">
-            <Image
+            <img
               src={activeImg}
               alt=""
-              className="object-cover w-full h-full aspect-square rounded-xl"
+              className="w-full h-full aspect-square object-cover rounded-xl"
             />
             <div className="flex flex-row justify-between h-24">
-              <Image
+              <img
                 src={images.img1}
                 alt=""
                 className="w-24 h-24 rounded-md cursor-pointer"
                 onClick={() => setActiveImage(images.img1)}
               />
-              <Image
+              <img
                 src={images.img2}
                 alt=""
                 className="w-24 h-24 rounded-md cursor-pointer"
                 onClick={() => setActiveImage(images.img2)}
               />
-              <Image
+              <img
                 src={images.img3}
                 alt=""
                 className="w-24 h-24 rounded-md cursor-pointer"
                 onClick={() => setActiveImage(images.img3)}
               />
-              <Image
+              <img
                 src={images.img4}
                 alt=""
                 className="w-24 h-24 rounded-md cursor-pointer"
@@ -63,74 +129,98 @@ export default function DetailMenu() {
               />
             </div>
           </div>
-          {/* ABOUT */}
           <div className="flex flex-col gap-4 lg:w-2/4">
             <div>
-              <h1 className="text-4xl font-bold">Nasi Goreng Spesial </h1>
+              <h1 className="text-4xl font-bold">{name || ''}</h1>
             </div>
-            <h6 className="text-2xl font-semibold text-emerald-600">
-              Rp. 25.000
+
+            <h6 className="text-emerald-600 text-2xl font-semibold">
+              Rp. {price || ''}
             </h6>
             <div className="flex flex-row gap-2">
               <p>Category : </p>
-              <p className="font-bold">Makanan</p>
+              <p className="font-bold">{category?.name || ''}</p>
             </div>
 
             <div className="flex flex-row gap-2">
               <p>Calories :</p>
-              <p className="font-bold">257 Calories</p>
+              <p className="font-bold">{calories || ''}</p>
             </div>
-            <p className="text-gray-700">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
-              nostrum amet at laboriosam laborum voluptatum veniam, reiciendis
-              modi minima et consequatur iure iusto totam voluptates dignissimos
-              corrupti cupiditate deleniti non.
-            </p>
+            <p className="text-gray-700">{description || ''}</p>
 
             <div className="flex flex-row items-center gap-12">
               <div className="flex flex-row items-center">
                 <button
-                  className="px-4 py-2 text-3xl bg-gray-200 rounded-lg text-emerald-800"
+                  className="bg-gray-200 py-2 px-4 rounded-lg text-emerald-800 text-3xl"
                   onClick={removeCountHandler}
                 >
                   -
                 </button>
-                <span className="px-6 py-4 rounded-lg"> {count}</span>
+                <span className="py-4 px-6 rounded-lg"> {count}</span>
 
                 <button
-                  className="px-4 py-2 text-3xl bg-gray-200 rounded-lg text-emerald-600"
+                  className="bg-gray-200 py-2 px-4 rounded-lg text-emerald-600 text-3xl"
                   onClick={addCountHandler}
                 >
                   +
                 </button>
               </div>
             </div>
-            <button className="h-full px-16 py-3 font-semibold text-white bg-emerald-600 rounded-xl">
+            <button className="bg-emerald-600 text-white font-semibold py-3 px-16 rounded-xl h-full">
               Add to Cart
             </button>
           </div>
         </div>
-        {/* Deskripsi */}
         <div>
           <div className="py-5">
             <h1 className="text-4xl font-bold">Deskripsi</h1>
           </div>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad maiores
-            officia, minus eligendi mollitia, natus architecto, asperiores
-            expedita dolore velit laudantium officiis consectetur laborum.
-            Similique suscipit laboriosam ratione repellat modi?
-          </p>
+          <p>{description || ''}</p>
         </div>
 
-        <div className="flex flex-row justify-between my-5 ">
+        <div className=" my-5 flex flex-row justify-between">
           <h1 className="text-4xl font-bold">Anda Mungkin Menyukai</h1>
 
-          <button className="px-16 py-3 font-semibold text-white bg-emerald-600 rounded-xl">
+          <button className="bg-emerald-600 text-white font-semibold py-3 px-16 rounded-xl">
             View More
           </button>
+        </div>
+
+        <div className="flex flex-wrap justify-center  gap-6">
+          {menus.map((menu) => (
+            <MenuCard
+              key={menu.id}
+              id={menu.id}
+              name={menu.name}
+              price={menu.price}
+              category={menu.category.name}
+              ratings={menu.ratings.map((rating) => rating.rating)}
+              menuImages={menu.menuImages}
+            />
+          ))}
         </div>
       </div>
     </div>
   );
-}
+};
+
+export const getServerSideProps: GetServerSideProps = async (
+  ctx: any,
+): Promise<any> => {
+  const { id } = ctx.query || {};
+  let resDataDetail: any;
+  try {
+    const { data } = await axios.get(
+      `${process.env.NEXT_PUBLIC_SERVICE_BASE}/menus/${Number(id)}`,
+    );
+    resDataDetail = data;
+    console.log(data);
+  } catch (error) {
+    console.error('Error during fetch: ', error);
+  }
+  return {
+    props: resDataDetail,
+  };
+};
+
+export default DetailMenu;
