0404975233e52ce77026f2cba0f5f38b8fa66efd
Okky Anggoro <anggorookky@gmail.com>
2023-07-18 08:22:40 +0800

    Revert "Merge Project into development branch"

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,15 @@
-import React from 'react';
 import { NextPage } from 'next';
 import Head from 'next/head';
-import Testimonies from '../components/Landingpage/Testimonies';
-import AboutUs from '@/components/Landingpage/AboutUs';
-import Landingpage from '@/components/Landingpage/Landingpage';
-import DownloadPage from '@/components/Landingpage/DownloadPage';
-import Features from '@/components/Landingpage/Features';
+import LandingPage from '../components/LandingPage';
 
 const Index: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Wareg</title>
+        <title>wareg</title>
         <meta name="description" content="Wareg." />
       </Head>
-      <div
-        style={{ backgroundColor: '#FFFEFC' }}
-        className="dark:bg-gray-900 font-poppins"
-      >
-        <Landingpage />
-        <AboutUs />
-        <Features />
-        <Testimonies />
-        <DownloadPage />
-      </div>
+      <LandingPage />
     </>
   );
 };
