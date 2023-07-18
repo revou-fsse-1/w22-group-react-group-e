770b855f8901940059bc37cfa3ad1c9ce09a4ad5
meselghea <122159476+meselghea@users.noreply.github.com>
2023-07-18 07:18:11 +0700

    update filter & pagination

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -3,7 +3,6 @@ import { getClient } from "../../sanity/lib/getClient";
 import { GetStaticPaths, GetStaticProps } from "next";
 import { SanityDocument } from "@sanity/client";
 import { urlForImage } from "../../sanity/lib/image";
-
 export const getStaticPaths: GetStaticPaths = async () => {
   const client = getClient();
   const slugsQuery = groq`*[_type == "post" && defined(slug.current)].slug.current`;
@@ -33,28 +32,32 @@ export default function BlogPostPage({ data }: { data: SanityDocument }) {
   const { title, body, mainImage, author, publishedAt } = data;
 
   return (
-    <div>
-      <h1>{title}</h1>
-      <a>Author: {author?.name}</a>
+    <div className="justify-center p-10 m-10">
+      <h1 className="p-10 text-4xl font-bold text-center bg-[#EEF6F4] text-emerald-950">{title}</h1>
+      {mainImage && (
+         <div className="flex justify-center">
+        <img
+          src={urlForImage(mainImage).url()}
+          alt={mainImage?.alt}
+          className="w-[500px] h-auto m-10"
+        />
+        </div>
+      )}
+      <div className="flex items-center text-gray-600">
+      <a className="p-3">Author: {author?.name}</a>
       <p>Published At: {new Date(publishedAt).toLocaleDateString("en-US", {
         year: "numeric",
         month: "long",
         day: "numeric",
       })}</p>
-      {mainImage && (
-        <img
-          src={urlForImage(mainImage).url()}
-          alt={mainImage?.alt}
-          className="w-full"
-        />
-      )}
+      </div>
     {body && (
         <div>
           {body.map((block: any, index: number) => {
             if (block._type === "block" && block.style === "h2") {
-              return <h2 key={index}>{block.children[0].text}</h2>;
+              return <h2 className="p-5 mb-3 text-2xl font-semibold" key={index}>{block.children[0].text}</h2>;
             } else if (block._type === "block") {
-              return <p key={index}>{block.children[0].text}</p>;
+              return <p className="justify-between p-3 mb-2 text-lg" key={index}>{block.children[0].text}</p>;
             }
             return null;
           })}
