ca68254fa68c30a220f8f65a7d2ebebfe0337336
anggr <anggorookky@gmail.com>
2023-07-20 23:05:11 +0800

    feat: Implement addToCart, removeFromCart, increaseQuantity, decreaseQuantity functions

--- components/CartModal.tsx
+++ components/CartModal.tsx
@@ -1,16 +1,18 @@
-import Link from 'next/link';
-import React, { useContext, useState } from 'react'; 
+import React, { useContext, useState } from 'react';
 import { HiShoppingCart } from 'react-icons/hi';
 import { GoTrash } from 'react-icons/go';
-import axios from 'axios';
 import Image from 'next/image';
-import { CartContext } from '../context/CartContext'; 
-
-
+import { CartContext } from '../context/CartContext';
 
 const CartModal: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const { cartItems, cartProducts } = useContext(CartContext); 
+  const {
+    cartProducts,
+    cartItems,
+    increaseQuantity,
+    decreaseQuantity,
+    removeFromCart,
+  } = useContext(CartContext);
 
   const openModal = () => {
     setIsOpen(true);
@@ -20,10 +22,10 @@ const CartModal: React.FC = () => {
     setIsOpen(false);
   };
 
-
-  
-
-  const total = cartProducts.reduce((sum, product) => sum + product.price, 0);
+  const total = cartProducts.reduce(
+    (sum, { product, quantity }) => sum + product.price * quantity,
+    0,
+  );
 
   return (
     <div>
@@ -44,14 +46,51 @@ const CartModal: React.FC = () => {
               <hr />
             </div>
 
-      
-            {cartProducts.map((product) => (
-              <div key={product.id}>{}</div>
+            {cartProducts.map(({ product, quantity }) => (
+              <div key={product.id}>
+                {/* <Image
+                  className="p-2"
+                  width={156}
+                  height={160}
+                  src={product.menuImages?.img1 ?? ''}
+                  alt={product.name}
+                /> */}
+
+                <div className="p-4">
+                  <p>{product.name}</p>
+                  <div className="flex flex-row items-center gap-12">
+                    <div className="flex flex-row items-center">
+                      <button
+                        className="bg-gray-200 px-2 rounded-lg text-emerald-800"
+                        onClick={() => decreaseQuantity(product.id)}
+                      >
+                        -
+                      </button>
+                      <span className="py-4 px-3 rounded-lg">{quantity}</span>
+                      <button
+                        className="bg-gray-200 px-2 rounded-lg text-emerald-800"
+                        onClick={() => increaseQuantity(product.id)}
+                      >
+                        +
+                      </button>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="p-4">
+                  <div>
+                    <button onClick={() => removeFromCart(product.id)}>
+                      <GoTrash className="text-red-600" />
+                    </button>
+                  </div>
+                  <p>Rp. {product.price}</p>
+                </div>
+              </div>
             ))}
 
             <div className="p-3 flex justify-between">
               <p>Total</p>
-              <p>Rp. {total}</p> {}
+              <p>Rp. {total}</p>
             </div>
 
             <button
--- context/CartContext.tsx
+++ context/CartContext.tsx
@@ -11,32 +11,75 @@ interface Order {
   };
 }
 
+interface CartProduct {
+  product: Order;
+  quantity: number;
+}
+
 interface CartContextProps {
   cartItems: number;
-  cartProducts: Order[];
-  addToCart: () => void;
-  addToCartServer: (product: Order) => void; 
+  cartProducts: CartProduct[];
+  addToCart: (product: Order) => void;
+  removeFromCart: (productId: number) => void;
+  increaseQuantity: (productId: number) => void;
+  decreaseQuantity: (productId: number) => void;
+  addToCartServer: (product: Order) => void;
 }
 
 export const CartContext = createContext<CartContextProps>({
   cartItems: 0,
   cartProducts: [],
   addToCart: () => {},
-  addToCartServer: () => {}, 
+  removeFromCart: () => {},
+  increaseQuantity: () => {},
+  decreaseQuantity: () => {},
+  addToCartServer: () => {},
 });
 
 export const CartProvider: React.FC = ({
   children,
 }: React.PropsWithChildren<{}>) => {
   const [cartItems, setCartItems] = useState(0);
-  const [cartProducts, setCartProducts] = useState<Order[]>([]);
+  const [cartProducts, setCartProducts] = useState<CartProduct[]>([]);
 
-  const addToCart = () => {
+  const addToCart = (product: Order) => {
     setCartItems((prevCount) => prevCount + 1);
+    setCartProducts((prevProducts) => [
+      ...prevProducts,
+      { product, quantity: 1 },
+    ]);
+  };
+
+  const increaseQuantity = (productId: number) => {
+    setCartProducts((prevProducts) =>
+      prevProducts.map((cartProduct) =>
+        cartProduct.product.id === productId
+          ? { ...cartProduct, quantity: cartProduct.quantity + 1 }
+          : cartProduct,
+      ),
+    );
+  };
+
+  const decreaseQuantity = (productId: number) => {
+    setCartProducts((prevProducts) =>
+      prevProducts.map((cartProduct) =>
+        cartProduct.product.id === productId && cartProduct.quantity > 1
+          ? { ...cartProduct, quantity: cartProduct.quantity - 1 }
+          : cartProduct,
+      ),
+    );
+  };
+
+  const removeFromCart = (productId: number) => {
+    setCartItems((prevCount) => prevCount - 1);
+    setCartProducts((prevProducts) =>
+      prevProducts.filter(
+        (cartProduct) => cartProduct.product.id !== productId,
+      ),
+    );
   };
 
   const addToCartServer = async (product: Order) => {
-    // Add this function
     const token = getCookie('token');
     try {
       await axios.post(
@@ -58,8 +101,7 @@ export const CartProvider: React.FC = ({
         },
       );
 
-      addToCart();
-      setCartProducts((prevProducts) => [...prevProducts, product]);
+      addToCart(product);
       alert('Item added to cart!');
     } catch (error) {
       console.error(error);
@@ -69,7 +111,15 @@ export const CartProvider: React.FC = ({
 
   return (
     <CartContext.Provider
-      value={{ cartItems, cartProducts, addToCart, addToCartServer }}
+      value={{
+        cartItems,
+        cartProducts,
+        addToCart,
+        removeFromCart,
+        increaseQuantity,
+        decreaseQuantity,
+        addToCartServer,
+      }}
     >
       {children}
     </CartContext.Provider>
