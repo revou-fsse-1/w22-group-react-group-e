4193530445f45637c1f6f7e610056a30274a1a30
anggr <anggorookky@gmail.com>
2023-07-21 20:19:40 +0800

    style: Remove unused imports and commented out code

--- pages/orders/index.tsx
+++ pages/orders/index.tsx
@@ -1,194 +1,47 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { getCookie } from '@/libs/cookies';
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-
-type Order = {
-  id: string;
-};
 
 export default function Orders() {
-  const [orders, setOrders] = useState([]); 
-
-  useEffect(() => {
-    fetchOrders().then((data) => setOrders(data));
-  }, []);
-
-  const fetchOrders = async () => {
-    const token = getCookie('token');
-    const userId = getCookie('userId');
-    try {
-      const response = await axios.get(
-        `https://w17-wareg.onrender.com/orders/user/${userId}`,
-        {
-          headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-            Authorization: `Bearer ${token}`,
-          },
-          withCredentials: true,
-        },
-      );
-      return response.data;
-    } catch (error) {
-      console.error(error);
-
-    }
-  };
-
-  return orders.map((order: Order) => (
-    <div
-      className="py-14 px-4 md:px-6 2xl:px-20 2xl:container 2xl:mx-auto"
-      key={order.id}
-    >
-      <div className="flex justify-start item-start space-y-2 flex-col">
-        <h1 className="text-3xl dark:text-white lg:text-4xl font-semibold leading-7 lg:leading-9 text-gray-800">
-          Order #{order.id}
-        </h1>
-        <p className="text-base dark:text-gray-300 font-medium leading-6 text-gray-600">
-          21st Mart 2021 at 10:34 PM
-        </p>
-      </div>
-      <div className="mt-10 flex flex-col xl:flex-row jusitfy-center items-stretch w-full xl:space-x-8 space-y-4 md:space-y-6 xl:space-y-0">
-        <div className="flex flex-col justify-start items-start w-full space-y-4 md:space-y-6 xl:space-y-8">
-          <div className="flex flex-col justify-start items-start dark:bg-gray-800 bg-gray-50 px-4 py-4 md:py-6 md:p-6 xl:p-8 w-full">
-            <p className="text-lg md:text-xl dark:text-white font-semibold leading-6 xl:leading-5 text-gray-800">
-              Customer’s Cart
-            </p>
-            <div className="mt-4 md:mt-6 flex flex-col md:flex-row justify-start items-start md:items-center md:space-x-6 xl:space-x-8 w-full">
-              {/* <div className="pb-4 md:pb-8 w-full md:w-40">
-                <Image
-                  className="w-full hidden md:block"
-                  src="https://i.ibb.co/84qQR4p/Rectangle-10.png"
-                  alt="dress"
-                />
-                <Image
-                  className="w-full md:hidden"
-                  src="https://i.ibb.co/L039qbN/Rectangle-10.png"
-                  alt="dress"
-                />
-              </div> */}
-              <div className="border-b border-gray-200 md:flex-row flex-col flex justify-between items-start w-full pb-8 space-y-4 md:space-y-0">
-                <div className="w-full flex flex-col justify-start items-start space-y-8">
-                  <h3 className="text-xl dark:text-white xl:text-2xl font-semibold leading-6 text-gray-800">
-                    Premium Quaility Dress
-                  </h3>
-                </div>
-                <div className="flex justify-between space-x-8 items-start w-full">
-                  <p className="text-base dark:text-white xl:text-lg leading-6">
-                    $36.00{' '}
-                  </p>
-                  <p className="text-base dark:text-white xl:text-lg leading-6 text-gray-800">
-                    01
-                  </p>
-                  <p className="text-base dark:text-white xl:text-lg font-semibold leading-6 text-gray-800">
-                    $36.00
-                  </p>
+  return (
+    <section style={{ lineHeight: '1.5' }} className="py-24 font-poppins">
+      <div className="container px-4 mx-auto">
+        <div className="max-w-2xl mx-auto text-center">
+          <span className="inline-block mx-auto mb-6">
+            <svg
+              width="54"
+              height="54"
+              viewBox="0 0 54 54"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M26.9999 0.333374C12.3066 0.333374 0.333252 12.3067 0.333252 27C0.333252 41.6934 12.3066 53.6667 26.9999 53.6667C41.6933 53.6667 53.6666 41.6934 53.6666 27C53.6666 12.3067 41.6933 0.333374 26.9999 0.333374ZM39.7466 20.8667L24.6266 35.9867C24.2532 36.36 23.7466 36.5734 23.2133 36.5734C22.6799 36.5734 22.1733 36.36 21.7999 35.9867L14.2533 28.44C13.4799 27.6667 13.4799 26.3867 14.2533 25.6134C15.0266 24.84 16.3066 24.84 17.0799 25.6134L23.2133 31.7467L36.9199 18.04C37.6933 17.2667 38.9733 17.2667 39.7466 18.04C40.5199 18.8134 40.5199 20.0667 39.7466 20.8667Z"
+                fill="#548776"
+              ></path>
+            </svg>
+          </span>
+          <span className="block mb-1 text-sm font-bold text-indigo-500">
+            SUKSES
+          </span>
+          <h3 className="text-2xl font-black mb-5">Ordermu sedang diproses</h3>
+          <p className="text-lg  mb-12">
+            Terima kasih telah memilih Wareg. Dengan menu yang berubah tiap
+            minggu, kami siapkan pengalaman makan yang baru dan menyenangkan.
+            Selamat menikmati.
+          </p>
+          <Link href="/menu">
+            <div className="group relative inline-block h-12 w-full xs:w-60 bg-blueGray-900 rounded-md">
+              <div className="absolute top-0 left-0 transform -translate-y-1 -translate-x-1 w-full h-full group-hover:translate-y-0 group-hover:translate-x-0 transition duration-300">
+                <div className="flex h-full w-full items-center justify-center bg-emerald-600 rounded-md">
+                  <span className="text-base font-black text-white">
+                    Lanjutkan Belanja
+                  </span>
                 </div>
               </div>
             </div>
-            <div className="mt-6 md:mt-0 flex justify-start flex-col md:flex-row items-start md:items-center space-y-4 md:space-x-6 xl:space-x-8 w-full">
-              <div className="w-full md:w-40">
-                {/* <Image
-                  className="w-full hidden md:block"
-                  src="https://i.ibb.co/s6snNx0/Rectangle-17.png"
-                  alt="dress"
-                />
-                <Image
-                  className="w-full md:hidden"
-                  src="https://i.ibb.co/BwYWJbJ/Rectangle-10.png"
-                  alt="dress"
-                /> */}
-              </div>
-              <div className="flex justify-between items-start w-full flex-col md:flex-row space-y-4 md:space-y-0">
-                <div className="w-full flex flex-col justify-start items-start space-y-8">
-                  <h3 className="text-xl dark:text-white xl:text-2xl font-semibold leading-6 text-gray-800">
-                    High Quaility Italic Dress
-                  </h3>
-                </div>
-                <div className="flex justify-between space-x-8 items-start w-full">
-                  <p className="text-base dark:text-white xl:text-lg leading-6">
-                    $20.00{' '}
-                  </p>
-                  <p className="text-base dark:text-white xl:text-lg leading-6 text-gray-800">
-                    01
-                  </p>
-                  <p className="text-base dark:text-white xl:text-lg font-semibold leading-6 text-gray-800">
-                    $20.00
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="flex justify-center flex-col md:flex-row items-stretch w-full space-y-4 md:space-y-0 md:space-x-6 xl:space-x-8">
-            <div className="flex flex-col px-4 py-6 md:p-6 xl:p-8 w-full bg-gray-50 dark:bg-gray-800 space-y-6">
-              <h3 className="text-xl dark:text-white font-semibold leading-5 text-gray-800">
-                Summary
-              </h3>
-              <div className="flex justify-center items-center w-full space-y-4 flex-col border-gray-200 border-b pb-4">
-                <div className="flex justify-between w-full">
-                  <p className="text-base dark:text-white leading-4 text-gray-800">
-                    Subtotal
-                  </p>
-                  <p className="text-base dark:text-gray-300 leading-4 text-gray-600">
-                    $56.00
-                  </p>
-                </div>
-              </div>
-              <div className="flex justify-between items-center w-full">
-                <p className="text-base dark:text-white font-semibold leading-4 text-gray-800">
-                  Total
-                </p>
-                <p className="text-base dark:text-gray-300 font-semibold leading-4 text-gray-600">
-                  $36.00
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="bg-gray-50 dark:bg-gray-800 w-full xl:w-96 flex justify-between items-center md:items-start px-4 py-6 md:p-6 xl:p-8 flex-col">
-          <h3 className="text-xl dark:text-white font-semibold leading-5 text-gray-800">
-            Customer
-          </h3>
-          <div className="flex flex-col md:flex-row xl:flex-col justify-start items-stretch h-full w-full md:space-x-6 lg:space-x-8 xl:space-x-0">
-            <div className="flex flex-col justify-start items-start flex-shrink-0">
-              <div className="flex justify-center w-full md:justify-start items-center space-x-4 py-8 border-b border-gray-200">
-                <div className="flex justify-start items-start flex-col space-y-2">
-                  <p className="text-base dark:text-white font-semibold leading-4 text-left text-gray-800">
-                    David Kent
-                  </p>
-                </div>
-              </div>
-
-              <div className="flex justify-center text-gray-800 dark:text-white md:justify-start items-center space-x-4 py-4 border-b border-gray-200 w-full">
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M19 5H5C3.89543 5 3 5.89543 3 7V17C3 18.1046 3.89543 19 5 19H19C20.1046 19 21 18.1046 21 17V7C21 5.89543 20.1046 5 19 5Z"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                  <path
-                    d="M3 7L12 13L21 7"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-                <p className="cursor-pointer text-sm leading-5 ">
-                  david89@gmail.com
-                </p>
-              </div>
-            </div>
-          </div>
+          </Link>
         </div>
       </div>
-    </div>
-  ));
+    </section>
+  );
 }
