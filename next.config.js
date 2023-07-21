2f1f86c3742dc6b21382aacdae9dd4ee4bc79247
meselghea <122159476+meselghea@users.noreply.github.com>
2023-07-21 13:14:03 +0700

    fix image sanity to config nextjs

--- next.config.js
+++ next.config.js
@@ -9,12 +9,17 @@ const nextConfig = {
       'sweetrip.id',
       'awsimages.detik.net.id',
       'global-uploads.webflow.com',
+      'cdn.sanity.io',
     ],
     async rewrites() {
       return [
         {
           source: '/:path*',
           destination: 'https://w17-wareg.onrender.com/:path*',
+        },
+        {
+          source: '/:path*',
+          destination: 'https://cdn.sanity.io/:path*',
         },
       ];
     },
